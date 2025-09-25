17495b37e447f76d1cee09e213762756d04b578e
carlos-andemberg <cinfoandemberg@gmail.com>
2025-09-25 10:19:33 -0300

    Melhoramento do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,12 @@ function Footer() {
     const currentYear = new Date().getFullYear(); // Pega o ano atual automaticamente
     return (
         <footer className="site-footer">
-            <p>&copy; {currentYear} AD Brás Piracicaba Tabuleiro - Reg. 17. Todos os direitos reservados.</p>
+            <p>
+                &copy; {currentYear} AD Brás Piracicaba Tabuleiro - Reg. 17.
+                <br className="mobile-only-break" />
+                {' '} {/* Garante um espaço no desktop quando a quebra de linha está escondida */}
+                Todos os direitos reservados.
+            </p>
         </footer>
     );
 }
